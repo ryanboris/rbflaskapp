1a090235b9c5cfc3f7f5f5ba2be697aef9a9d521
Ryan Boris <ryan.boris@gmail.com>
2020-04-03 20:18:55 -0400

    :construction: Update deps and app

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return "Hello, Index!"
+    return 'Hello!'
+
+
+@app.route('/<name>')
+def hello_name(name):
+    return f'Hello, {name}!'
 
 
 if __name__ == '__main__':
